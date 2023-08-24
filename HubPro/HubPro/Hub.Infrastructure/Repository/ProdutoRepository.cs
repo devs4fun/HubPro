a579ecde64ccd05101a2dedbd68bcd847c051bf4
eneasdev <lucasmartins_eu@hotmail.com>
2023-08-23 22:10:01 -0300

    *Adicionado crud à ProdutoRepository *Criado interface de contrato para ProdutoRepository

--- HubPro/HubPro/Hub.Infrastructure/Repository/ProdutoRepository.cs
+++ HubPro/HubPro/Hub.Infrastructure/Repository/ProdutoRepository.cs
@@ -1,6 +1,39 @@
-﻿namespace HubPro.Hub.Infrastructure.Repository
+﻿using HubPro.Hub.Domain.Models;
+using HubPro.Hub.Infrastructure.Interfaces;
+
+namespace HubPro.Hub.Infrastructure.Repository
 {
-    public class ProdutoRepository
+    public class ProdutoRepository : IProdutoRepository
     {
+        readonly ContextHub _contextHub;
+        public ProdutoRepository(ContextHub contextHub)
+        {
+            _contextHub = contextHub;
+        }
+
+        public IEnumerable<Produto> BuscarTodos()
+        {
+            return _contextHub.Produtos;
+        }
+
+        public Produto? BuscarPorId(int id)
+        {
+            return _contextHub.Produtos.FirstOrDefault(p => p.id == id);
+        }
+
+        public void Cadastrar(Produto produto)
+        {
+            _contextHub.Add(produto);
+        }
+
+        public void Atualizar(Produto produto)
+        {
+            _contextHub.Update(produto);
+        }
+
+        public void Deletar(int id)
+        {
+            _contextHub.Remove(id);
+        }
     }
 }
