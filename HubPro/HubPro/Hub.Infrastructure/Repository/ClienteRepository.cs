a408c6f5b5c7e7631d9b172bf76afa3d068372bf
Robson Junior <robsonjunior1994@gmail.com>
2023-09-16 19:58:24 -0300

    Permitindo atualização de cliente.

--- HubPro/HubPro/Hub.Infrastructure/Repository/ClienteRepository.cs
+++ HubPro/HubPro/Hub.Infrastructure/Repository/ClienteRepository.cs
@@ -1,6 +1,7 @@
 ﻿using HubPro.Hub.Domain.Models;
 using HubPro.Hub.Infrastructure.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HubPro.Hub.Infrastructure.Repository
 {
@@ -12,11 +13,19 @@ namespace HubPro.Hub.Infrastructure.Repository
             _contextHub = contextHub;
         }
 
+        public void Atualizar(Cliente cliente)
+        {
+            _contextHub.Update(cliente);
+            _contextHub.SaveChanges();
+        }
+
         public Cliente Buscar(string request)
         {
             int.TryParse(request, out int id);
 
-            return _contextHub.Cliente.FirstOrDefault(x => x.Celular == request || x.Nome == request || x.Id == id);
+            return _contextHub.Cliente
+                .Include(c => c.Endereco)
+                .FirstOrDefault(x => x.Celular == request || x.Nome == request || x.Id == id);
         }
 
         public void Cadastrar(Cliente cliente)
