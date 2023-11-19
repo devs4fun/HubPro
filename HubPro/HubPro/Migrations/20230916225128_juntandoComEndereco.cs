83caf733890b56a8adfcc4c984214805fca945e2
Robson Junior <robsonjunior1994@gmail.com>
2023-11-18 21:06:37 -0300

    Criando o frontend, criei todo o crud de clientes.

---
+++ HubPro/HubPro/Migrations/20230916225128_juntandoComEndereco.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HubPro.Migrations
+{
+    /// <inheritdoc />
+    public partial class juntandoComEndereco : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Documento = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Celular = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Aniversario = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Valor = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Quantidade = table.Column<double>(type: "float", nullable: false),
+                    Ativo = table.Column<bool>(type: "bit", nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Endereco",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Rua = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Bairro = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Numero = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Estado = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cep = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Complemento = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Observacoes = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Endereco", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Endereco_Cliente_Id",
+                        column: x => x.Id,
+                        principalTable: "Cliente",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Endereco");
+
+            migrationBuilder.DropTable(
+                name: "Produtos");
+
+            migrationBuilder.DropTable(
+                name: "Cliente");
+        }
+    }
+}
---
+++ HubPro/WebpAppHubPro/Controllers/ClienteController.cs
@@ -0,0 +1,122 @@
+﻿using Azure;
+using HubPro.Hub.API.DTOs.Request;
+using HubPro.Hub.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using static System.Net.WebRequestMethods;
+
+namespace WebpAppHubPro.Controllers
+{
+    public class ClienteController : Controller
+    {
+        HttpClient _httpClient;
+        public ClienteController()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:7189/");
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        }
+        // GET: ClienteController
+        public async Task<ActionResult> Index(int pg)
+        {
+            if (pg <= 0)
+                    pg = 1;
+
+            var reponse = await _httpClient.GetAsync("/api/cliente/BuscarVarios?pg="+pg);
+            string content = await reponse.Content.ReadAsStringAsync();
+            List<ClienteRequest> listaClientes = JsonConvert.DeserializeObject<List<ClienteRequest>>(content);
+            return View("Index", listaClientes);
+        }
+
+        // GET: ClienteController/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View("Create");
+        }
+
+        // POST: ClienteController/Create
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(ClienteRequest request)
+        {
+            var bodyJson = System.Text.Json.JsonSerializer.Serialize(request);
+            var content = new StringContent(bodyJson, Encoding.UTF8, "application/json");
+            try
+            {
+                var response = await _httpClient.PostAsync("/api/cliente", content);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ClienteController/Edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            var reponse = await _httpClient.GetAsync("/api/cliente/BuscarPorIdentificador?request="+id);
+            string content = await reponse.Content.ReadAsStringAsync();
+            ClienteRequest Cliente = JsonConvert.DeserializeObject<ClienteRequest>(content);
+            return View("ClientEdit", Cliente);
+        }
+
+        // POST: ClienteController/Edit/5
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(ClienteRequest request)
+        {
+            var bodyJson = System.Text.Json.JsonSerializer.Serialize(request);
+            var content = new StringContent(bodyJson, Encoding.UTF8, "application/json");
+            try
+            {
+                var response = await _httpClient.PutAsync("/api/cliente", content);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // POST: ClienteController/Delete/5
+        [HttpPost]
+       // [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(int clienteId)
+        {
+            try
+            {
+                var reponse = await _httpClient.DeleteAsync("/api/cliente?id="+clienteId);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Search([FromQuery] string busca)
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync("/api/cliente/BuscarPorIdentificador?request="+ busca);
+                string content = await response.Content.ReadAsStringAsync();
+                ClienteRequest Cliente = JsonConvert.DeserializeObject<ClienteRequest>(content);
+                return View("ClientEdit", Cliente);
+            }
+            catch
+            {
+                return RedirectToAction(nameof(Index));
+            }
+        }
+    }
+}
